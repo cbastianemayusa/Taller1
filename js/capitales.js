380e349d3f6e8d0c7fd49e0a843fc21518430377
cbastianemayusa <cbastianemayusa@gmail.com>
2021-09-28 09:28:36 -0500

    primer commit

---
+++ js/capitales.js
@@ -0,0 +1,116 @@
+
+// let departamento = prompt("ingrese el departamento");
+
+// let depa = prompt("ingresa el departamento ");
+// const departamento=() =>{
+//     // let depa = prompt("ingresa el departamento ");ama
+
+//     if(depa == "amazonas")
+//     alert(" Su capital es : Leticia");
+// }
+
+const Depa=() => {
+    
+
+
+let departamento= prompt("ingrese el departamento")
+
+ if(departamento == "amazonas"){
+     alert("La capital es Leticia");
+ }
+  else if (departamento == " antioquia"){
+     alert("La capital es"+" medellin");
+ }
+ else if (departamento == "arauca"){
+     alert("La capital es"+" arauca");
+ }
+ else if (departamento == "atlantico"){
+    alert("La capital es"+" Barranquilla");
+} 
+else if (departamento == "boyaca"){
+    alert("La capital es"+" Tunja");
+} 
+else if (departamento == "caldas"){
+    alert("La capital es"+" Manizales");
+}
+else if (departamento == "caqueta"){
+    alert("La capital es"+" florencia");
+} 
+else if (departamento == "casanare"){
+    alert("La capital es"+" yopal");
+}
+else if (departamento == "cauca"){
+    alert("La capital es"+" popayan");
+}
+else if (departamento == "cesar"){
+    alert("La capital es"+" Valledupar");
+}
+else if (departamento == "choco"){
+    alert("La capital es"+" Quibdo");
+}
+else if (departamento == "cordoba"){
+    alert("La capital es"+" Monteria");
+}
+else if (departamento == "cundinamarca"){
+    alert("La capital es"+" Bogota");
+}
+else if (departamento == "guainia"){
+    alert("La capital es"+" Irinida");
+}
+else if (departamento == "guaviare"){
+    alert("La capital es"+" San José del Guaviare");
+}
+else if (departamento == "huila"){
+    alert("La capital es"+" Neiva");
+}
+else if (departamento == "guajira"){
+    alert("La capital es"+" Riohacha");
+}
+else if (departamento == "magdalena"){
+    alert("La capital es"+" Santa Marta");
+}
+
+else if (departamento == "meta"){
+    alert("La capital es"+" Villavicencio");
+}
+else if (departamento == "nariño"){
+    alert("La capital es"+" Pasto");
+}
+else if (departamento == "norte de santander"){
+    alert("La capital es"+" cucuta");
+}
+else if (departamento == " putumayo"){
+    alert("Mocoa");
+}
+else if (departamento == "quindio"){
+    alert("La capital es"+" Armenia");
+}
+else if (departamento == "risaralda"){
+    alert("La capital es"+" Pereira");
+}
+else if (departamento == "san andres y providencia"){
+    alert("La capital es"+" San Andres");
+}
+else if (departamento == "santander"){
+    alert("La capital es"+" Bucaramanga");
+}
+else if (departamento == "sucre"){
+    alert("La capital es"+" sincelejo");
+}
+else if (departamento == "tolima"){
+    alert("La capital es"+" Ibague");
+}
+else if (departamento == "valle del cauca"){
+    alert("La capital es"+" Cali");
+}
+else if (departamento == "vaupes"){
+    alert("La capital es"+" Mitú");
+}
+else if (departamento == "vichada") {
+    alert("La capital es"+" Puerto Carreño");
+
+} else {
+    alert(" Colombia no tiene departamentos con ese nombre");
+} 
+
+}
